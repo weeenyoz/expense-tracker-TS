7d53b7fd121a27bcf51402cc508bb54fa0f47e5a
Wenyao <peterleewenyaoalpha@gmail.com>
2020-04-01 17:30:56 +0800

    add types to deleteTransaction & getTransactions methods

--- client/src/context/Actions.ts
+++ client/src/context/Actions.ts
@@ -1,13 +1,17 @@
 import axios from 'axios';
+import { AddAction, DeleteAction, GetTransactionsAction } from './GlobalState';
 
-export const deleteTransaction = (id: number) => {
+export const deleteTransaction = async (id: number): Promise<DeleteAction> => {
+    const result = await axios.delete(`/api/v1/transactions/${id}`);
+    console.log('result in deleteTransactions: ', result);
+
     return {
         type: 'DELETE_TRANSACTION',
         payload: id,
     };
 };
 
-export const getTransactions = async () => {
+export const getTransactions = async (): Promise<GetTransactionsAction> => {
     try {
         const result = await axios.get('/api/v1/transactions');
         const { transactions } = result.data;
